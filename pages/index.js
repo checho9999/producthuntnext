e6a3cad35011d2d6ef6b22bc4f8cd0c29d2f2038
Checho <checho9999@gmail.com>
2020-04-25 07:59:01 -0300

    En el commit anterior, para el caso de alguna referencia a NuevoProducto(layout) es en realidad a NuevoProducto(pages); En el componente index(pages) incluimos el FirebaseContext(firebase); En index(pages) agregamos useEffect para poder monitorear y obtener(ante cualquier actualizacion del omponente) los productos del usuario(accediendo a sus documentos) desde la base de datos con snapshot.docs, y tambien agreagamos useState para actualizar el state con los productos obtenidos anteriormente; Creamos el componente DetallesProducto(components) para poder mostrar el detalle de cada producto recuperado desde la base de datos; Aplicamos estilo en el componente DetallesProducto con styled components(@emotion/styled) creando sus propios estilos(Producto, DescripcionProducto, Titulo, TextoDescripcion, Comentarios, Imagen y Votos); Instalamos la dependencia date-fns para poder formatear en idioma español(formatDistanceToNow y locale) la fecha de creacion del documento recibida desde la base de datos.

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,61 @@
-import React from 'react';
+import React, { useEffect, useState, useContext } from 'react'
 import Layout from '../components/layout/Layout';
+import DetallesProducto from '../components/layout/DetallesProducto';
+import { FirebaseContext } from '../firebase';
 
 //const Heading = styled.h1`color: red`...ejemplo con styled components
 //<style jsx>{`h1{color: red}`}</style>...ejemplo css en js
-const Home = () => (
-  <div>
-    <Layout>
-      <h1>Inicio</h1>
-    </Layout>
-  </div>
-)
+//onSnapshot y su funcion manejarSnapshot, es lo que realmente accede a los datos y nos permite iterarlos
+const Home = () => {
 
-export default Home;
+  //Actualizacion del state de lo productos traidos desde la base de datos
+  const [productos, guardarProductos] = useState([]);
+
+  //State de firebase
+  const { firebase } = useContext(FirebaseContext);
+
+  //Monitoreamos el renderizado del componente, para ir mostrando los productos del usuario
+  useEffect(() => {
+    //Definimos el orden en que se van a traer los datos
+    const obtenerProductos = () => {
+        firebase.db.collection('productos').orderBy('creado', 'desc').onSnapshot(manejarSnapshot)
+    }
+    //Obtenemos todos los productos desde la base de datos
+    obtenerProductos();
+  }, []);
+
+  //Manejamos el Snapshot, accediendo e iterando sobre los datos
+  //El doc.id es para iterar en el map y el doc.data() para traer todo el registo de cada doc.id
+  function manejarSnapshot(snapshot) {
+      const productos = snapshot.docs.map(doc => {
+        return {
+          id: doc.id,
+          ...doc.data()
+        }
+      });
+      //console.log(productos)
+      //Colocamos los productos en el state
+      guardarProductos(productos);
+  }
+
+  return (    
+    <div>
+      <Layout>
+        <div className='listado-productos'>
+            <div className='contenedor'>
+              <ul className='bg-white'>
+                  {productos.map(producto => (
+                      <DetallesProducto
+                          key={producto.id}
+                          producto={producto}
+                      />
+                  ))}
+              </ul>
+            </div>
+        </div>
+      </Layout>
+    </div>
+  )
+}
+
+export default Home
